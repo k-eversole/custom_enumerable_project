3c3a0913cde184f2b73c8be96dc809e99b3b9c92
Semaphore <kristen.m.eversole@gmail.com>
2022-05-17 19:34:53 -0500

    Wrote custom enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,80 @@
+# frozen_string_literal: true
+
+# Standard enumerables
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    index = 0
+    my_each do |value|
+      yield [value, index]
+      index += 1
+    end
+  end
+
+  def my_any?
+    my_each do |value|
+      return true if yield(value) == true
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |value|
+      return false if yield(value) == true
+    end
+    true
+  end
+
+  def my_all?
+    my_each do |value|
+      return false if yield(value) == false
+    end
+    true
+  end
+
+  def my_select
+    selected = []
+    my_each do |value|
+      selected << value if yield(value) == true
+    end
+    selected
+  end
+
+  def my_count
+    return length unless block_given?
+
+    count = 0
+    my_each do |value|
+      count += 1 if yield(value) == true
+    end
+    count
+  end
+
+  def my_map
+    mapped = []
+    my_each do |value|
+      mapped << yield(value)
+    end
+    mapped
+  end
+
+  def my_inject(acc)
+    i = 0
+    while i < length
+      acc = yield *acc, self[i]
+      i += 1
+    end
+    acc
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
+# base enumerable used for methods in Enumerable
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    while i < length
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 end
